1a1d78322a0d1991f61963ef34dc09baa0e1aeb6
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2022-11-30 13:09:57 -1000

    DEL: dynaconf

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
-import os
 import setuptools
-from setuptools import setup
 
-setup(
+
+setuptools.setup(
     name="oleas",
     description="OLEAS readout",
     python_requires=">=3.9",
-    install_requires=[
-        'dynaconf==3.1.11'
-    ],
-    entry_points = {
-        'console_scripts': [
-            'oleas_sensors=oleas.scripts.read_sensors:main',
-            'oleas_sweep=oleas.scripts.sweep:main',
-        ],
-    },
+    install_requires=[],
     packages=setuptools.find_packages(),
 )
