45d5de1a401ca79e6d2e1107ad18cc40f7b130e9
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2022-12-01 13:16:06 -1000

    ADD: mcp4728

--- oleas/mcp4725.py
+++ oleas/mcp4725.py
@@ -1,26 +1,20 @@
 from naludaq.devices.i2c_device import I2CDevice
 
-DEFAULT_ADDRESS = 0b01100010
+DEFAULT_ADDRESS = 0b1100010
 
 
-class MCP4725:
+class Mcp4725:
 
-    def __init__(self, board, address: int = DEFAULT_ADDRESS) -> None:
+    def __init__(self, board, address: int = DEFAULT_ADDRESS):
         self._device = I2CDevice(board, address)
 
-    def set_value(self, val: int) -> None:
-        """Set 16-bit value (0-65535). Lower four bits are discarded."""
-        if not 0 <= val <= 65535:
-            raise ValueError('Value must be 0 to 65535')
-        self.set_raw_value(val >> 4)
-
-    def set_normalized_value(self, val: float) -> None:
+    def set_normalized_value(self, val: float):
         """Set normalized value (0.0 - 1.0)"""
         if not 0.0 <= val <= 1.0:
             raise ValueError('Value must be 0.0 to 1.0')
-        self.set_raw_value(int(val * 4095.0))
+        self.set_value(int(val * 4095.0))
 
-    def set_raw_value(self, val: int) -> None:
+    def set_value(self, val: int):
         """Set 12-bit value (0 - 4095)"""
         if not 0 <= val <= 4095:
             raise ValueError('Value must be 0 to 4095')
---
+++ oleas/mcp4728.py
@@ -0,0 +1,50 @@
+from naludaq.devices.i2c_device import I2CDevice
+
+
+DEFAULT_ADDRESS = 0b1100010
+SINGLE_WRITE_COMMAND = 0b01011000
+
+
+class Mcp4728:
+    """Controller for the MCP4728"""
+
+    def __init__(self, board, address: int = DEFAULT_ADDRESS):
+        self._device = I2CDevice(board, address)
+
+    def set_normalized_value(self, channel: int, val: float, vref: int=1, gain: int=1):
+        """Set normalized value (0.0 - 1.0)
+
+        Args:
+            channel (int): channel number (0-4)
+            value (int): 12-bit value (0 - 4095)
+            vref (int): 0 (VDD) or 1 (internal 2.048 V)
+            gain (int): 1 (output 0.0 to 2.048 V) or 2 (output 0.0 to 4.096 V).
+        """
+        if not 0.0 <= val <= 1.0:
+            raise ValueError('Value must be 0.0 to 1.0')
+        self.set_value(channel, int(val * 4095.0), vref, gain)
+
+    def set_value(self, channel: int, val: int, vref=0, gain=1):
+        """Set 12-bit value (0 - 4095)
+
+        Args:
+            channel (int): channel number (0-4)
+            value (int): 12-bit value (0 - 4095)
+            vref (int): 0 (VDD) or 1 (internal 2.048 V)
+            gain (int): 1 (output 0.0 to 2.048 V) or 2 (output 0.0 to 4.096 V).
+        """
+        if not 0 <= val <= 4095:
+            raise ValueError('Value must be 0 to 4095')
+        if channel not in range(4):
+            raise ValueError('Channel must be 0-4')
+        if vref not in [0, 1]:
+            raise ValueError('VREF must be 0 or 1')
+        if gain not in [1, 2]:
+            raise ValueError('Gain must be 1 or 2')
+
+        buf = bytearray(3)
+        buf[0] = SINGLE_WRITE_COMMAND | (channel << 1)
+        buf[1] = (vref << 7) | (gain << 4) | ((val >> 8) & 0xF)
+        buf[2] = val & 0xFF
+
+        self._device.send_write_command(buf, check_ack=False)
