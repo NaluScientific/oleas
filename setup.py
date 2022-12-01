96c727732d06edb82bb68cb0659e54718972552e
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2022-12-01 13:42:45 -1000

    CHG: move scripts again

--- setup.py
+++ setup.py
@@ -7,10 +7,4 @@ setuptools.setup(
     python_requires=">=3.9",
     install_requires=[],
     packages=setuptools.find_packages(),
-    entry_points = {
-        'console_scripts': [
-            'show_boards=oleas.scripts.show_boards:main',
-        ],
-    },
-
 )
